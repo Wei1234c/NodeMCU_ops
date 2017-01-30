cb78197784fa91d60f44d2eac3ea574f677d7555
Wei Lin <Wei1234c@gmail.com>
2017-01-31 00:27:27 +0800

    update MicroPython firmware.

--- platforms/MicroPython/codes/blink10times.py
+++ platforms/MicroPython/codes/blink10times.py
@@ -4,4 +4,4 @@
 # ##### http://maxlai.cc/2016/10/22/micropython-lab1-blink-led/
 
 # import led
-# led.blink(led.on_board_led, times = 3)
+# led.blink(led.on_board_led, times = 10)
--- platforms/MicroPython/firmwares/upload_MicroPython_firmware_to_NodeMCU.bat
+++ platforms/MicroPython/firmwares/upload_MicroPython_firmware_to_NodeMCU.bat
@@ -1,4 +1,4 @@
-python ..\..\..\tools\esptool.py --port COM15 flash_id
-python ..\..\..\tools\esptool.py --port COM15 erase_flash
-python ..\..\..\tools\esptool.py --port COM15 --baud 115200 write_flash --flash_size=32m 0x00000 MicroPython_ESP8266-20161110-v1.8.6.bin
+python D:\Python\scripts\esptool.py --port COM15 flash_id
+python D:\Python\scripts\esptool.py --port COM15 erase_flash
+python D:\Python\scripts\esptool.py --port COM15 --baud 115200 write_flash --flash_size=32m 0x00000 MicroPython_ESP8266-20170108-v1.8.7.bin
 PAUSE
