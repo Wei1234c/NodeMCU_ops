1f8eb9470fea0be294afe3cf3847ada46210004a
Wei Lin <Wei1234c@gmail.com>
2017-06-08 14:18:58 +0800

    20170608

--- platforms/MicroPython/codes/blink10times.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# # 透過 MicroPython 閃爍 NodeMCU 上的LED燈
-# ##### http://maxlai.cc/2016/10/22/micropython-lab1-blink-led/
-
-# import led
-# led.blink(led.on_board_led, times = 10)
--- platforms/MicroPython/firmwares/upload_MicroPython_firmware_to_NodeMCU.bat
+++
@@ -1,4 +0,0 @@
-python D:\Python\scripts\esptool.py --port COM15 flash_id
-python D:\Python\scripts\esptool.py --port COM15 erase_flash
-python D:\Python\scripts\esptool.py --port COM15 --baud 115200 write_flash --flash_size=32m 0x00000 MicroPython_ESP8266-20170108-v1.8.7.bin
-PAUSE
